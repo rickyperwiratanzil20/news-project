a1ad8000588b96c1dcc4496b8afc22cbdb635a72
rickyperwiratanzil20 <chenyongan12345@gmail.com>
2023-06-05 12:10:17 +0700

    Setup Phpmyadmin Port

--- news-backend/app/Http/Controllers/RegisterController.php
+++ news-backend/app/Http/Controllers/RegisterController.php
@@ -8,17 +8,11 @@ class RegisterController extends Controller
 {
     public function register(Request $request)
     {
-        // Validasi data input
-
         $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:8',
         ]);
-
-        // Simpan data ke database atau lakukan tindakan lainnya
-
-        // Kembalikan respon JSON
 
         return response()->json(['message' => 'Registration successful']);
     }
--- news-frontend/src/pages/Register/index.js
+++ news-frontend/src/pages/Register/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './register.scss';
 import {RegisterBg} from '../../assets';
 import { Button, Gap, Input, Link } from '../../components';
@@ -6,6 +6,29 @@ import {useNavigate} from 'react-router-dom';
 
 const Register = () => {
     const navigate = useNavigate();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch('/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name, email, password }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    };
+
     return (
         <div className='main-page'>
             <div className='left'>
@@ -13,15 +36,17 @@ const Register = () => {
             </div>
             <div className='right'>
                 <p className='title'>Register</p>
-                <Input label="Full Name" placeholder="Full Name"/>
-                <Gap height={18}/>
-                <Input label="Email" placeholder="Email"/>
-                <Gap height={18}/>
-                <Input label="Password" placeholder="Password"/>
-                <Gap height={20}/>
-                <Button title='Register' onClick={() => navigate('/login')}/>
-                <Gap height={60}/>
-                <Link title="Kembali Ke Login" onClick={() => navigate('/login')}/>
+                <form onSubmit={handleSubmit}>
+                    <Input label="Full Name" placeholder="Full Name" type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+                    <Gap height={18}/>
+                    <Input label="Email" placeholder="Email" type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <Gap height={18}/>
+                    <Input label="Password" placeholder="Password" type="text" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <Gap height={20}/>
+                    <Button title='Register' type="submit"/>
+                    <Gap height={60}/>
+                    <Link title="Kembali Ke Login" onClick={() => navigate('/login')}/>
+                </form>
             </div>
         </div>
     )
